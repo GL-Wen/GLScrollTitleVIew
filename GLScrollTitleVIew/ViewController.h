9681595952337457cad34bc201df06b5a9561cd7
LIVE1016 <william@william.local>
2016-03-20 17:34:03 +0800

    Initial Commit

---
+++ GLScrollTitleVIew/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GLScrollTitleVIew
+//
+//  Created by William on 16/3/20.
+//  Copyright © 2016年 William. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GLScrollTitleVIew/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GLScrollTitleVIew
+//
+//  Created by William on 16/3/20.
+//  Copyright © 2016年 William. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
